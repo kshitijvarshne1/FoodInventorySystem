a1eec2689b87dbe639f53d595b9aef7947514d98
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-09 16:01:50 +0530

    create FruitItem.java

---
+++ src/base_class/FruitItem.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 09-Mar-21
+ *   Time: 4:01 PM
+ *   File: FruitItem.java
+ */
+
+package base_class;
+
+public class FruitItem {
+}
+

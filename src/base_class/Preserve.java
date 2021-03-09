a3112b1abdc47276927235a414c8414bccb3fed5
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-10 00:03:58 +0530

    create Preserve.java

---
+++ src/base_class/Preserve.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 10-Mar-21
+ *   Time: 12:03 AM
+ *   File: Preserve.java
+ */
+
+package base_class;
+
+public class Preserve {
+}
+

b4141d884ba770b34782d275d1b5ca5701e8b18b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-10 01:01:46 +0530

    create Sweeteners.java

---
+++ src/base_class/Sweeteners.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 10-Mar-21
+ *   Time: 1:01 AM
+ *   File: Sweeteners.java
+ */
+
+package base_class;
+
+public class Sweeteners {
+}
+

b4918d2f3fc59406a1f296126ee6fb81d3900375
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-09 15:50:20 +0530

    add getter and setter

--- src/base_class/FoodItem.java
+++ src/base_class/FoodItem.java
@@ -22,5 +22,44 @@ public class FoodItem {
         this.itemCost = itemCost;
     }
 
+    public int getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(int itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public float getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(float itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public float getItemCost() {
+        return itemCost;
+    }
+
+    public void setItemCost(float itemCost) {
+        this.itemCost = itemCost;
+    }
 }
 

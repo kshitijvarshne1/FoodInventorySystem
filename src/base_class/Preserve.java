f2ea87e73851fe038fceb6115b9e2b3c253d91b8
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-10 01:00:20 +0530

    add addItem method

--- src/base_class/Preserve.java
+++ src/base_class/Preserve.java
@@ -7,6 +7,45 @@
 
 package base_class;
 
-public class Preserve {
+import java.util.Scanner;
+
+public class Preserve extends FoodItem {
+    private int jarSize;
+
+    public Preserve(int itemCode, String itemName, float itemPrice, int quantityInStock, float itemCost, int jarSize) {
+        super(itemCode, itemName, itemPrice, quantityInStock, itemCost);
+        this.jarSize = jarSize;
+    }
+
+    public Preserve(int jarSize) {
+        this.jarSize = jarSize;
+    }
+    public Preserve() {
+        this.jarSize = 0;
+    }
+
+    public int getJarSize() {
+        return jarSize;
+    }
+
+    public void setJarSize(int jarSize) {
+        this.jarSize = jarSize;
+    }
+
+    @Override
+    public String toString() {
+        return "Preserve{" +
+                "jarSize=" + jarSize +
+                '}';
+    }
+    public Preserve additem(){
+        FoodItem obj =super.addItem();
+        System.out.println("Enter the size of the jar in millilitres: ");
+        Scanner sc= new Scanner(System.in);
+        int size = sc.nextInt();
+        Preserve p = new Preserve(obj.getItemCode(),obj.getItemName(),obj.getItemPrice(),obj.getQuantityInStock(),obj.getItemCost(),size);
+        return p;
+    }
+
 }
 

aab569cfe3a1acf66293555f60fe1da1948e865f
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-09 22:26:01 +0530

    create Inventory.java

---
+++ src/base_class/Inventory.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 09-Mar-21
+ *   Time: 10:25 PM
+ *   File: Inventory.java
+ */
+
+package base_class;
+
+public class Inventory {
+}
+

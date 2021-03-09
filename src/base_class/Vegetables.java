93c0b997c0d8a3c568d5f5b95929574f862c35a9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-03-09 15:55:57 +0530

    create Vegetables.java

---
+++ src/base_class/Vegetables.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 09-Mar-21
+ *   Time: 3:54 PM
+ *   File: Vegetables.java
+ */
+
+package base_class;
+
+public class Vegetables {
+}
+
